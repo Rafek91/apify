7d957b96812c34a52bb19c48e92ff36cff869a1e
Rafek91 <radek.chlud@gmail.com>
2024-01-26 10:31:04 +0100

    adding notes

--- apiRequest.js
+++ apiRequest.js
@@ -8,7 +8,7 @@ let max = 10000
 
 let products = []
 
-const distinctProducts = (responseProducts, productPool) => {
+const distinctProducts = (responseProducts, productPool) => { ////filtering products by ID. Multiple products could have the same price, so I need to be sure all products are loaded with no duplicities
     const filteredNewProducts = responseProducts.filter(product => !productPool.some(item => item.product_id === product.product_id));
     console.log("added products", filteredNewProducts)
     return filteredNewProducts
@@ -18,7 +18,7 @@ const loadProducts = (minimalValue, maximalValue) => {
     const productApiResponse = listProducts(minimalValue, maximalValue)
     console.log("api count", productApiResponse.count, "api total", productApiResponse.total)
 
-    if (productApiResponse.count == productApiResponse.total) {
+    if (productApiResponse.count == productApiResponse.total) { // if the product response count is the same as total amount of products I can return from the recursive function
         const fetchedProducts = productApiResponse.products
         products = [...products, ...distinctProducts(fetchedProducts, products)]
         return products
